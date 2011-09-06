ba9bd9205e706357cae787b5e4e55d262a1d2471
Wouter Bos <wouter@thebrightlines.com>
2011-09-06 18:08:51 +0300

    Option for making paging links real links with an array of URL's Enable paging by hovering over a numbered link

--- Website/javascript/jquery.onopager.core.js
+++ Website/javascript/jquery.onopager.core.js
@@ -202,6 +202,8 @@
         active: true,
         enableClick: true,
         labels: [],
+        links: [],
+        enableHover: false,
         hideThreshold: -1
       },
       pageByArrowKey: {
@@ -379,29 +381,64 @@
       var pageTotal = animation.getPagesLength();
       var html = '';
       var label;
+      var href;
+      var hrefVoid = 'javascript:void(0)';
 
       for (var i = 0; i < pageTotal; i++) {
+        // Set label text
         if (config.pageByNumber.labels &&
             config.pageByNumber.labels.length > i) {
           label = config.pageByNumber.labels[i];
         } else {
           label = i + 1;
         }
-        html += '<a' + EMPTY_HREF + '>' + label + '</a>';
+        
+        // Set label link
+        if (config.pageByNumber.links &&
+            config.pageByNumber.links.length > i) {
+          href = config.pageByNumber.links[i];
+        } else {
+          href = hrefVoid;
+        }
+        
+        html += '<a href="' + href + '"><span><span>' + label + '</span></span></a>';
       }
 
       pageByNumber.html(html);
+      
+      setPageEvent();
 
-      if (config.pageByNumber.enableClick == true) {
-        pageByNumber.find('a').each(function(index) {
-          $(this).click(function() {
-            if ($(this).hasClass('onoPager_active') == false) {
-              page(index, 0);
-            }
+      function setPageEvent() {
+        var eventTypes = '';
+        var pageTimeout;
+        if (config.pageByNumber.enableClick == true) {
+          pageByNumber.find('a').each(function(index) {
+            $(this).click(function(event) {
+              var isActive = $(this).hasClass('onoPager_active');
+              if (isActive == false && $(this).attr('href') == hrefVoid) {
+                page(index);
+              }
+            });
           });
-        });
-      } else {
-        pageByNumber.find('a').addClass('onoPager_readonly');
+        }
+        if (config.pageByNumber.enableHover == true) {
+          pageByNumber.find('a').each(function(index) {
+            $(this).mouseenter(function(event) {
+              if ($(this).hasClass('onoPager_active') == false) {
+                clearTimeout(pageTimeout)
+                pageTimeout = setTimeout(function() { page(index); root.addClass('onoPager_disabled'); }, 150);
+              }
+            });
+            $(this).mouseleave(function(event) {
+              root.removeClass('onoPager_disabled');
+            });
+          });
+        }
+        
+        if (config.pageByNumber.enableClick == false &&
+            config.pageByNumber.enableHover == false) {
+          pageByNumber.find('a').addClass('onoPager_readonly');
+        }
       }
     }
 
